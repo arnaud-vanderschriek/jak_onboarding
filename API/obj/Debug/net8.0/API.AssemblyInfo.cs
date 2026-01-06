19c3687c7493dd9cb0f6e7e4db480142ce7fa8e7
arnaud <arnaud.vanderschrieck@gmail.com>
2026-01-06 16:59:27 +0100

    add some changes in API

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df96322cbe876632f61e5be3718b1f99215d15ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eadf40226051f84cf652e3a1be2adc1cd4f25da")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
