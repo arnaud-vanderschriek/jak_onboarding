6e050fa27242373144fb424ac4903087e6c7621c
arnaud <arnaud.vanderschrieck@gmail.com>
2026-01-09 16:11:03 +0100

    Adding the shopping cart feaure: 86 - Adding a basket component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77a1f123e5ed8452bc7e0d087555b8b310b027e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8120ead66b4877de2899852b5da0f04df1667d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,6 +1,6 @@
 import { DarkMode, LightMode, ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, LinearProgress, List, ListItem, Toolbar, Typography } from "@mui/material";
-import { NavLink } from "react-router";
+import { Link, NavLink } from "react-router";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { setDarkMode } from "./uiSlice";
 
@@ -55,7 +55,7 @@ export default function NavBar() {
           </List>
 
           <Box sx={{ display: 'flex', alignItems: 'center'}}>
-            <IconButton size="large" sx={{ color: 'inherit' }}>
+            <IconButton component={Link} to='/basket' size="large" sx={{ color: 'inherit' }}>
               <Badge badgeContent='4' color="secondary">
                 <ShoppingCart />
               </Badge>
