b84c9d598c7c4ec6c9574babd4b12c8aa45ba323
arnaud <arnaud.vanderschrieck@gmail.com>
2026-01-12 09:29:13 +0100

    Adding the shopping cart feature: 89 - Invalidating RTK Query cache

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160a808268ad3c084176317d610361f6023e81a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9149f3cc4e3916d23e816f6cf89848e720316469")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
